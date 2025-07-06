1500fcce796c3dd53e8d2b96b9598a2dca9b113c
Neitan0 <127685007+Neitan0@users.noreply.github.com>
2025-07-06 19:04:38 -0300

    Initial Commit

---
+++ WebQuiz/WebQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebQuizApp.swift
+//  WebQuiz
+//
+//  Created by Natanael nogueira on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WebQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebQuizTests/WebQuizTests.swift
@@ -0,0 +1,16 @@
+//
+//  WebQuizTests.swift
+//  WebQuizTests
+//
+//  Created by Natanael nogueira on 06/07/25.
+//
+
+import Testing
+
+struct WebQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
